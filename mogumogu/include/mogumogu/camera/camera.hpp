b43ea9ff7792fa000997883c494de00d336fd00f
yumcyawiz <hashdolf1923@gmail.com>
2020-11-27 00:56:18 +0900

    impl camera constructor

--- mogumogu/include/mogumogu/camera/camera.hpp
+++ mogumogu/include/mogumogu/camera/camera.hpp
@@ -2,9 +2,22 @@
 #define _CAMERA_H
 
 #include "mogumogu/ray.hpp"
+#include "mogumogu/vec3.hpp"
 
 class Camera {
+ private:
+  Vec3 camPos;
+  Vec3 camForward;
+  Vec3 camRight;
+  Vec3 camUp;
+
  public:
+  Camera(const Vec3& _camPos, const Vec3& _camForward)
+      : camPos(_camPos), camForward(normalize(_camForward)) {
+    camRight = cross(camForward, Vec3(0, 1, 0));
+    camUp = cross(camRight, camForward);
+  }
+
   virtual bool sampleRay(Ray& ray, float& pdf) const = 0;
 };
 
---
+++ mogumogu/include/mogumogu/camera/pinhole.hpp
@@ -0,0 +1,6 @@
+#ifndef _PINHOLE_H
+#define _PINHOLE_H
+#include "mogumogu/camera/camera.hpp"
+
+class PinholeCamera : public Camera {}
+#endif
