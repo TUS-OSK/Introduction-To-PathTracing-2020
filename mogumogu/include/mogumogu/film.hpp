0d4a2571d79c07672bb95d3ff78a441517988f8f
yumcyawiz <hashdolf1923@gmail.com>
2020-11-27 03:05:32 +0900

    slim film class

--- mogumogu/include/mogumogu/film.hpp
+++ mogumogu/include/mogumogu/film.hpp
@@ -6,41 +6,19 @@
 
 class Film {
  public:
-  unsigned int width;
-  unsigned int height;
-  float width_length;
-  float height_length;
-
-  float* pixels;
-
-  Film(unsigned int _width, unsigned int _height, float _width_length = 0.036,
-       float _height_length = 0.024);
-  ~Film();
-
-  // delete copy constructor
-  Film(const Film& film) = delete;
-
-  // move constructor
-  Film(Film&& film);
-
-  // move = operator
-  Film& operator=(Film&& film);
+  Film(unsigned int _width, unsigned int _height, float _width_length = 0.036f,
+       float _height_length = 0.024f);
 
   Vec3 getPixel(unsigned int i, unsigned int j) const;
   void setPixel(unsigned int i, unsigned int j, const Vec3& rgb);
 
  private:
-  float getR(unsigned int i, unsigned int j) const;
-  float getG(unsigned int i, unsigned int j) const;
-  float getB(unsigned int i, unsigned int j) const;
+  unsigned int width;
+  unsigned int height;
+  float width_length;
+  float height_length;
 
-  void setR(unsigned int i, unsigned int j, float value);
-  void setG(unsigned int i, unsigned int j, float value);
-  void setB(unsigned int i, unsigned int j, float value);
-
-  unsigned int computeRIndex(unsigned int i, unsigned int j) const;
-  unsigned int computeGIndex(unsigned int i, unsigned int j) const;
-  unsigned int computeBIndex(unsigned int i, unsigned int j) const;
+  std::vector<float> pixels;
 };
 
 #endif
--- mogumogu/src/film.cpp
+++ mogumogu/src/film.cpp
@@ -2,53 +2,18 @@
 
 Film::Film(unsigned int _width, unsigned int _height, float _width_length, float _height_length)
     : width(_width), height(_height), width_length(_width_length), height_length(_height_length) {
-  pixels = new float[3 * width * height];
-}
-
-Film::~Film() { delete[] pixels; }
-
-Film::Film(Film&& film) {
-  pixels = film.pixels;
-  film.pixels = nullptr;
-}
-
-Film& Film::operator=(Film&& film) {
-  delete[] film.pixels;
-  pixels = film.pixels;
-  film.pixels = nullptr;
-  return *this;
+  pixels.resize(3 * width * height);
 }
 
 Vec3 Film::getPixel(unsigned int i, unsigned int j) const {
-  return Vec3(getR(i, j), getG(i, j), getB(i, j));
+  const float R = pixels[3 * i + 3 * width * j];
+  const float G = pixels[3 * i + 3 * width * j + 1];
+  const float B = pixels[3 * i + 3 * width * j + 2];
+  return Vec3(R, G, B);
 }
 
 void Film::setPixel(unsigned int i, unsigned int j, const Vec3& rgb) {
-  setR(i, j, rgb.x());
-  setG(i, j, rgb.y());
-  setB(i, j, rgb.z());
-}
-
-float Film::getR(unsigned int i, unsigned int j) const { return pixels[computeRIndex(i, j)]; }
-float Film::getG(unsigned int i, unsigned int j) const { return pixels[computeGIndex(i, j)]; }
-float Film::getB(unsigned int i, unsigned int j) const { return pixels[computeBIndex(i, j)]; }
-
-void Film::setR(unsigned int i, unsigned int j, float value) {
-  pixels[computeRIndex(i, j)] = value;
-}
-void Film::setG(unsigned int i, unsigned int j, float value) {
-  pixels[computeGIndex(i, j)] = value;
-}
-void Film::setB(unsigned int i, unsigned int j, float value) {
-  pixels[computeBIndex(i, j)] = value;
-}
-
-unsigned int Film::computeRIndex(unsigned int i, unsigned int j) const {
-  return 3 * i + 3 * width * j;
-}
-unsigned int Film::computeGIndex(unsigned int i, unsigned int j) const {
-  return 3 * i + 3 * width * j + 1;
-}
-unsigned int Film::computeBIndex(unsigned int i, unsigned int j) const {
-  return 3 * i + 3 * width * j + 2;
+  pixels[3 * i + 3 * width * j] = rgb.x();
+  pixels[3 * i + 3 * width * j] = rgb.y();
+  pixels[3 * i + 3 * width * j] = rgb.z();
 }
