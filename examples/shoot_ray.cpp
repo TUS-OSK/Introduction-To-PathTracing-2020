dced613d3d5c4553e3f393983e9b4c38590e5724
yumcyawiz <hashdolf1923@gmail.com>
2020-11-27 21:17:32 +0900

    fix shoot_ray example

--- examples/shoot_ray.cpp
+++ examples/shoot_ray.cpp
@@ -1,23 +1,24 @@
+#include <numbers>
+
 #include "mogumogu/camera/film.hpp"
 #include "mogumogu/camera/pinhole.hpp"
 
 int main() {
   constexpr int width = 512;
   constexpr int height = 512;
-  Film film(512, 512);
-  PinholeCamera camera(Vec3(0, 0, 0), Vec3(0, 0, 1));
+  const auto film = std::make_shared<Film>(512, 512);
+  PinholeCamera camera(Vec3(0, 0, 0), Vec3(0, 0, 1), film, 0.5f * 3.14f);
 
   for (int j = 0; j < height; ++j) {
     for (int i = 0; i < width; ++i) {
       const float u = (2.0f * i - width) / height;
       const float v = (2.0f * j - height) / height;
-      const Vec3 pFilm = film.computePosition(Vec2(u, v));
-      const auto [ray, pdf] = camera.sampleRay(Vec3(pFilm.x(), pFilm.y(), 0));
-      film.setPixel(i, j, 0.5f * (ray.direction + 1.0f));
+      const auto [ray, pdf] = camera.sampleRay(Vec2(u, v));
+      camera.film->setPixel(i, j, 0.5f * (ray.direction + 1.0f));
     }
   }
 
-  film.writePPM("output.ppm");
+  camera.film->writePPM("output.ppm");
 
   return 0;
 }
