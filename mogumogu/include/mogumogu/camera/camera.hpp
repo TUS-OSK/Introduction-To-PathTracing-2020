d120b2d95a6b99d0f3a02056f00282dc455267f7
yumcyawiz <hashdolf1923@gmail.com>
2020-11-27 18:50:47 +0900

    impl pinhole camera

--- mogumogu/include/mogumogu/camera/camera.hpp
+++ mogumogu/include/mogumogu/camera/camera.hpp
@@ -1,29 +1,27 @@
 #ifndef _CAMERA_H
 #define _CAMERA_H
 #include <memory>
+#include <tuple>
 
-#include "mogumogu/camera/film.hpp"
 #include "mogumogu/core/ray.hpp"
 #include "mogumogu/core/vec2.hpp"
 #include "mogumogu/core/vec3.hpp"
 
 class Camera {
- private:
+ protected:
   Vec3 camPos;
   Vec3 camForward;
   Vec3 camRight;
   Vec3 camUp;
 
-  const Film* film;
-
  public:
-  Camera(const Vec3& _camPos, const Vec3& _camForward, const Film* _film)
-      : camPos(_camPos), camForward(normalize(_camForward)), film(_film) {
+  Camera(const Vec3& _camPos, const Vec3& _camForward)
+      : camPos(_camPos), camForward(normalize(_camForward)) {
     camRight = cross(camForward, Vec3(0, 1, 0));
     camUp = cross(camRight, camForward);
   }
 
-  virtual bool sampleRay(const Vec2& uv, Ray& ray, float& pdf) const = 0;
+  virtual std::tuple<Ray, float> sampleRay(const Vec3& pFilm) const = 0;
 };
 
 #endif
--- mogumogu/include/mogumogu/camera/pinhole.hpp
+++ mogumogu/include/mogumogu/camera/pinhole.hpp
@@ -1,15 +1,18 @@
 #ifndef _PINHOLE_H
 #define _PINHOLE_H
+
 #include "mogumogu/camera/camera.hpp"
 
-class PinholeCamera : public Camera {
+class PinholeCamera final : public Camera {
  public:
-  PinholeCamera(const Vec3& _camPos, const Vec3& _camForward, const Film* _film)
-      : Camera(_camPos, _camForward, _film) {}
+  PinholeCamera(const Vec3& _camPos, const Vec3& _camForward) : Camera(_camPos, _camForward) {}
 
-  bool sampleRay(const Vec2& uv, Ray& ray, float& pdf) const { return false; }
+  std::tuple<Ray, float> sampleRay(const Vec3& pFilm) const override {
+    const Vec3 pPinhole = camPos + pinholeDistance * camForward;
+    return {Ray(pPinhole, normalize(pPinhole - camPos)), 1.0f};
+  }
 
  private:
-  static constexpr float pinhole_distance = 1;
+  static constexpr float pinholeDistance = 1.0f;
 };
 #endif
